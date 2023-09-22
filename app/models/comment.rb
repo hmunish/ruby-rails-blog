b4ab088a131966ba3e2a21802fee495dd644eda6
Munish <hmunish5@gmail.com>
2023-09-22 14:36:33 +0200

    Add class name for models associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User'
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
     post.comments.count
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User'
   def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
     post.likes.count
