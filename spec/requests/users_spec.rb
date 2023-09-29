8b7652fcc1db0815990f7a8e8d3b0001d026e127
Munish <hmunish5@gmail.com>
2023-09-29 11:05:27 +0200

    Add specs for controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET/index' do
+    before do
+      get users_url
+    end
+
+    it 'repond with http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('users/index')
+    end
+
+    it 'responds with the correct body' do
+      expect(response.body).to include('list of users')
+    end
+  end
+
+  # show action
+  describe 'GET/show' do
+    before do
+      get user_url(1)
+    end
+
+    it 'repond with http success' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('users/show')
+    end
+
+    it 'responds with the correct body' do
+      expect(response.body).to include('details of user')
+    end
   end
 end
